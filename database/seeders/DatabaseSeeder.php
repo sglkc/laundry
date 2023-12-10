05bb040c63b973151ed334ea7734bfa25e07d2af
Seya <31957516+sglkc@users.noreply.github.com>
2023-12-10 19:03:04 +0700

    init commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Filament\Commands\MakeUserCommand as FilamentMakeUserCommand;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
