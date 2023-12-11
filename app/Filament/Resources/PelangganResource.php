32ee3de0d77a71f33c85abcd8763f24c131c09d8
Error404393 <kenasyah12@gmail.com>
2023-12-11 11:26:59 +0700

    InpuT Pelanggan

---
+++ app/Filament/Resources/PelangganResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PelangganResource\Pages;
+use App\Filament\Resources\PelangganResource\RelationManagers;
+use App\Models\Pelanggan;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PelangganResource extends Resource
+{
+    protected static ?string $model = Pelanggan::class;
+
+    protected static ?string $pluralModelLabel = 'pelanggan';
+
+    protected static ?string $slug = 'pelanggan';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                ->schema([
+                    TextInput::make('nama_pelanggan')
+                        ->label('Nama Pelanggan')
+                        ->required(),
+
+                    TextInput::make('alamat')
+                        ->label('Alamat')
+                        ->required(),
+
+                    TextInput::make('no_telepon')
+                        ->label('No Telepon')
+                        ->required()
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_pelanggan'),
+                TextColumn::make('alamat'),
+                TextColumn::make('no_telepon')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPelanggans::route('/'),
+            'create' => Pages\CreatePelanggan::route('/create'),
+            'edit' => Pages\EditPelanggan::route('/{record}/edit'),
+        ];
+    }
+}
