27645561cb43ccae67229f56d3b2c1c8bedf45c5
Error404393 <kenasyah12@gmail.com>
2024-01-05 15:00:18 +0700

    final

---
+++ resources/views/index.blade.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Laundry Express</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png " rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Variables CSS Files. Uncomment your preferred color scheme -->
+  <link href="assets/css/variables.css" rel="stylesheet">
+  <!-- <link href="assets/css/variables-blue.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-green.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-orange.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-purple.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-red.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-pink.css" rel="stylesheet"> -->
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: HeroBiz
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top" data-scrollto-offset="0">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="assets/img/logo.png" alt="">
+        <h1>Laundry Express<span>.</span></h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="index.html#features">Service</a></li>
+          <li><a class="nav-link scrollto" href="index.html#team">Contact</a></li>
+          <li><a class="nav-link scrollto" href="index.html#contact">Our Place</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle d-none"></i>
+      </nav><!-- .navbar -->
+
+      <a class="btn-getstarted scrollto" href="/login">LOG IN</a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <section id="hero-animated" class="hero-animated d-flex align-items-center">
+    <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
+      <img src="assets/img/hero-carousel/landing.png  " class="img-fluid animated">
+      <h2>Welcome to <span>Laundry Express</span></h2>
+      <p>Selamat datang di LaundryExpress! Cuci pakaian jadi 
+        lebih mudah dengan layanan antar-jemput kami. Pemesanan
+        online cepat dan hasil cuci terbaik.</p>
+    </div>
+  </section>
+
+  <main id="main">
+
+    <!-- ======= Features Section ======= -->
+    <section class="features">
+      <div class="container" data-aos="fade-up">
+
+        <ul class="nav nav-tabs row gy-4 d-flex">
+
+          <li class="nav-item col-6 col-md-4 col-lg-2">
+            <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#tab-1">
+              <i class="bi bi-binoculars color-cyan"></i>
+              <h4>Wash and Dry</h4>
+            </a>
+          </li><!-- End Tab 1 Nav -->
+
+          <li class="nav-item col-6 col-md-4 col-lg-2">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-2">
+              <i class="bi bi-box-seam color-indigo"></i>
+              <h4>Pick Up</h4>
+            </a>
+          </li><!-- End Tab 2 Nav -->
+
+          <li class="nav-item col-6 col-md-4 col-lg-2">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-3">
+              <i class="bi bi-brightness-high color-teal"></i>
+              <h4>Delivery</h4>
+            </a>
+          </li><!-- End Tab 3 Nav -->
+
+          <li class="nav-item col-6 col-md-4 col-lg-2">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-4">
+              <i class="bi bi-command color-red"></i>
+              <h4>Guarantee</h4>
+            </a>
+          </li><!-- End Tab 4 Nav -->
+
+        </ul>
+
+        <div class="tab-content">
+
+          <div id="features" class="tab-pane active show" id="tab-1">
+            <div class="row gy-4">
+              <div class="col-lg-8 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
+                <h3>Wash And Dry</h3>
+                <p class="fst-italic">
+                  Tim ahli kami yang berpengalaman akan menangani pakaian Anda dengan cermat, memastikan hasil bersih dan segar setiap kali.
+                  Proses ini didesain untuk menghemat waktu dan energi Anda, sehingga Anda dapat fokus pada hal-hal yang lebih penting dalam hidup Anda.
+                </p>
+                <ul>
+                  <li><i class="bi bi-check-circle-fill"></i> Menggunakan bahan yang aman untuk pakaian.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Wangi setiap saat dengan berbagai macam pilihan parfum.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Pakaian siap pakai karena sudah termasuk setrika dengan rapih dan nyaman.</li>
+                </ul>
+                <p>
+                  Nikmati pengalaman pencucian dan pengeringan yang tanpa stres dengan layanan Wash and Dry kami. Percayakan pakaian Anda kepada kami, dan rasakan perbedaannya.
+                  Hemat waktu, nikmati kebersihan, dan rasakan kenyamanan dengan layanan kami yang dapat diandalkan dan berkualitas tinggi
+                </p>
+              </div>
+              <div class="col-lg-4 order-1 order-lg-2 text-center" data-aos="fade-up" data-aos-delay="200">
+                <img src="assets/img/features-1.svg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End Tab Content 1 -->
+
+          <div class="tab-pane" id="tab-2">
+            <div class="row gy-4">
+              <div class="col-lg-8 order-2 order-lg-1">
+                <h3>Pick Up</h3>
+                <p>
+                  Dengan layanan pick up kami, Anda tidak perlu repot-repot datang ke tempat pencucian pakaian. Tim kami yang profesional akan 
+                  menjemput pakaian kotor Anda langsung di depan pintu rumah atau tempat kerja Anda, sesuai dengan jadwal yang telah Anda tentukan. 
+                  Ini memberi Anda kebebasan untuk fokus pada kegiatan lain yang lebih penting dalam harian Anda.
+                </p>
+                <p class="fst-italic">
+                  Pick Up Pakaian Anda, Tinggalkan Keprihatinan. Kami Hadir untuk Mempermudah Hidup Anda.
+                </p>
+                <ul>
+                  <li><i class="bi bi-check-circle-fill"></i> Cepat dan aman.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Satsett joss langsung sampai rumah.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Jadwal jemput yang fleksibel.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Layanan pelanggan yang responsif.</li>
+                </ul>
+              </div>
+              <div class="col-lg-4 order-1 order-lg-2 text-center">
+                <img src="assets/img/features-2.svg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End Tab Content 2 -->
+
+          <div class="tab-pane" id="tab-3">
+            <div class="row gy-4">
+              <div class="col-lg-8 order-2 order-lg-1">
+                <h3>Delivery</h3>
+                <p>
+                  Dengan layanan Laundry Delivery kami, Anda dapat melepaskan diri dari kerumitan mencuci dan mengeringkan pakaian.
+                  Cukup beri tahu kami kapan dan di mana kami harus menjemput pakaian kotor Anda, dan tim kami yang handal akan mengambilnya dengan cepat.
+                  Pakaian Anda kemudian akan dicuci dengan cermat, menggunakan produk berkualitas tinggi, dan dikembalikan dengan kebersihan yang menyegarkan.
+                </p>
+                <ul>
+                  <li><i class="bi bi-check-circle-fill"></i> Kemudahan pengambilan dan pengembalian.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> kualitas layanan yang konsisten dan responsif.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Langsung gass sampai rumah buat nganterin.</li>
+                </ul>
+                <p class="fst-italic">
+                  Jadikan hidup Anda lebih mudah dengan Laundry Delivery kami. Kami mengantarkan kebersihan dan kenyamanan langsung ke depan pintu Anda,
+                  sehingga Anda dapat fokus pada hal-hal yang benar-benar penting dalam hidup Anda.
+                </p>
+              </div>
+              <div class="col-lg-4 order-1 order-lg-2 text-center">
+                <img src="assets/img/features-3.svg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End Tab Content 3 -->
+
+          <div class="tab-pane" id="tab-4">
+            <div class="row gy-4">
+              <div class="col-lg-8 order-2 order-lg-1">
+                <h3>Guarantee</h3>
+                <p>
+                  Layanan yang kami sediakan akan dilindungi dengan garansi yang kami miliki. Kami mengutamakan kenyamanan dan kualitas untuk pengguna, 
+                  sehingga pelayanan yang kami berikan dapat berkesan dan menjadi sahabat hati Anda dalam memilih layanan laundry terbaik untuk pakaian
+                  yang Anda gunakan.
+                </p>
+                <p class="fst-italic">
+                  Kepuasan pelanggan adalah kepuasan kami juga.
+                </p>
+                <ul>
+                  <li><i class="bi bi-check-circle-fill"></i> Garansi Kebersihan Terjamin.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Jaminan Perlindungan Warna dan Kualitas Bahan.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Jadwal Pengambilan dan Pengembalian yang Tepat Waktu.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Jaminan Keamanan dan Privasi.</li>
+                  <li><i class="bi bi-check-circle-fill"></i> Kepuasan Pelanggan yang Diutamakan.</li>
+                </ul>
+              </div>
+              <div class="col-lg-4 order-1 order-lg-2 text-center">
+                <img src="assets/img/features-4.svg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End Tab Content 4 -->
+
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>Contact</h2>
+          <p>Hubungi kami di Laundry Express untuk memberikan sentuhan kebersihan pada pakaian Anda! Tim layanan pelanggan kami yang ramah dan responsif siap membantu dengan pertanyaan, jadwal pengambilan, atau permintaan khusus Anda.</p>
+        </div>
+
+        <div class="row gy-5">
+
+          <div class="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
+            <div class="team-member">
+              <div class="member-img">
+                <a href="https://wa.me/qr/A5M6DUTIYSZHO1"><img src="assets/img/team/contact1.jpg" class="img-fluid" alt="whatsaap icon"></a>
+              </div>
+              <div class="member-info">
+                <h4>Whatsapp</h4>   
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+          <div class="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="400">
+            <div class="team-member">
+              <div class="member-img">
+                <a href="https://www.instagram.com/"><img src="assets/img/team/instagram.avif" class="img-fluid" alt="instagram logo"></a>
+              </div>
+              <div class="member-info">
+                <h4>Instagram</h4>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+          <div class="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="600">
+            <div class="team-member">
+              <div class="member-img">
+                <a href="https://www.linkedin.com/"><img src="assets/img/team/linkedin.png" class="img-fluid" alt="linked in logo"></a>
+              </div>
+              <div class="member-info">
+                <h4>Linked.In</h4>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-header">
+          <h2>Our Place</h2>
+          <p>Jangan lupa gunakan layanan kami dan segera Kunjungi lokasi kami.</p>
+        </div>
+
+      </div>
+
+      <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31685.202033832917!2d107.68682332688952!3d-6.932339504066418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c2d0ff77b9ab%3A0xc76f0100832a41eb!2sPanyileukan%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1704415979940!5m2!1sen!2sid" frameborder="0" allowfullscreen></iframe>
+      </div><!-- End Google Maps -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-legal text-center">
+      <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+        <div class="d-flex flex-column align-items-center align-items-lg-start">
+          <div class="copyright">
+            &copy; Copyright <strong><span>HeroBiz X Laundry Express</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+
+        <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+          <a href="https://www.whatsapp.com/" class="twitter"><i class="bi bi-whatsapp"></i></a>
+          <a href="https://www.instagram.com/" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="https://www.linkedin.com/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div>
+
+      </div>
+    </div>
+
+  </footer><!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
